860c3f090afefe6457606c61b662a8aa3f366c46
harrison.cabral <cabral.hs@gmail.com>
2016-02-19 14:33:10 -0200

    index

--- FinanceiroWeb/src/financeiro/web/UsuarioBean.java
+++ FinanceiroWeb/src/financeiro/web/UsuarioBean.java
@@ -1,9 +1,12 @@
 package financeiro.web;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import financeiro.usuario.Usuario;
+import financeiro.usuario.UsuarioRN;
 
 @ManagedBean(name="usuarioBean")
 @RequestScoped
@@ -11,6 +14,27 @@ public class UsuarioBean {
 
 	private Usuario usuario = new Usuario();
 	private String confirmarSenha;
+	
+	public String novo(){
+		this.usuario = new Usuario();
+		this.usuario.setAtivo(true);
+		return "usuario";
+	}
+	
+	public String salvar(){
+		FacesContext contex = FacesContext.getCurrentInstance();
+		String senha = this.usuario.getSenha();
+		if (!senha.equals(this.confirmarSenha)) {
+			FacesMessage facemessage = new FacesMessage("A senha não foi confirmada corretamente");
+			contex.addMessage(null, facemessage);
+			return null;
+		}
+		
+		UsuarioRN UsuarioRN = new UsuarioRN();
+		UsuarioRN.salvar(this.usuario);
+		return "usuarioSucesso";
+	}
+	
 	
 	public Usuario getUsuario() {
 		return usuario;
